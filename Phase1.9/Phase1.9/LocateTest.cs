8bdaf97dd2a795deb6995ddf0b3bd4331e127a25
swathibylla <swathibylla@gmail.com>
2023-12-05 10:57:05 +0530

    files

---
+++ Phase1.9/Phase1.9/LocateTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace Phase1._9
+{
+    [TestClass]
+    public class LocateTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var driver = new FirefoxDriver())
+            {
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://www.simplilearn.com/");
+
+                new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(
+                     d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
+
+              //  var searchBox = driver.FindElementById("header_srch");
+               // Assert.IsNotNull(searchBox);
+               // searchBox.SendKeys("ASP.NET");
+               // var searchButton = driver.FindElementByClassName("login");
+                //searchButton.Click();
+
+                new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(
+                   d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
+
+
+                var pageTitle = driver.PageSource.Contains("Learning on Simplilearn");
+                Assert.IsNotNull(pageTitle);
+
+                //var footerText = driver.FindElementByXPath("//footer[@class='footer']");
+                //Assert.IsNotNull(footerText);
+            }
+        }
+    }
+
+}
+  
---
+++ Phase1.9/Phase1.9/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Phase1.9")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Phase1.9")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1671f938-9846-4f56-8516-f264df6b8112")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
