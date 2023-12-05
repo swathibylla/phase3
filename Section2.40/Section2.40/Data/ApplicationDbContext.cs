5bdd0647aa7ffc8541ccde9aec9758de868760f4
swathibylla <swathibylla@gmail.com>
2023-12-05 10:44:42 +0530

    files

---
+++ Section2.40/Section2.40/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Section2._40.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Section2.40/Section2.40/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Section2._40.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
