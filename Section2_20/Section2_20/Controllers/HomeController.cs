2a99e41b71bc735f73c7387d52892cb7e2437a0e
swathibylla <swathibylla@gmail.com>
2023-12-05 10:37:00 +0530

    changes

---
+++ Section2_20/Section2_20/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Section2_20.Models;
+using System.Diagnostics;
+using System.Text;
+
+
+namespace Section2_20.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(StudentModel model)
+        {
+            StringBuilder sb = new StringBuilder("Form data:\n");
+            sb.Append(model.Name + ", " + model.Address + "," + model.Class + "," + model.Email);
+            return Content(sb.ToString());
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+
+        }
+            public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Section2_20/Section2_20/Models/StudentModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Section2_20.Models
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Class { get; set; }
+        public string Email { get; set; }
+    }
+
+}
+
