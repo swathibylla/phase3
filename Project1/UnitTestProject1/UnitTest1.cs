3c7a304593237b08042aa1176104fd9fe36ea915
swathibylla <swathibylla@gmail.com>
2023-12-05 10:49:24 +0530

    files

---
+++ Project1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Moq;
+using NUnit;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var teacherMock = new Mock<Teacher>();
+            teacherMock.Setup(t => t.GetName()).Returns("Kavya");
+            //Act
+            var result = teacherMock.Object.GetName();
+
+            //Assert
+            Assert.AreEqual("kavya", result);
+        }
+    }
+}
---
+++ Project1/WindowLibrary/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowLibrary
+{
+   
+    public class Teacher
+    {
+        public string Name { get; set; }
+        public string Subjecttaught { get; set; }
+        
+        public class Student
+        {
+            public string Name { get; set; }
+            public int Grade { get; set; }
+           
+            public class Subject
+            {
+                public string SubjectName { get; set; }
+                public int HoursPerWeek { get; set; }
+            }
+            
+        }
+    }
+}
+
+
+        
