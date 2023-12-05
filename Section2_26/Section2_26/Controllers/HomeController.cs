3ccebade97806a27e71448900f93661f9fe4f4ad
swathibylla <swathibylla@gmail.com>
2023-12-05 10:34:45 +0530

    files

---
+++ Section2_26/Section2_26/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Section2_26.Models;
+using System.Diagnostics;
+
+namespace Section2_26.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            if (Request.Query["t"] == "")
+                ViewData["message"] = "Please select a list type";
+            else if (Request.Query["t"] == "students")
+            {
+                ViewData["stype"] = "students";
+                ViewData["message"] = "List Of Students";
+                List<String> list = new List<string>();
+                for (int i = 1; i <= 10; i++)
+                {
+                    list.Add("Student " + i.ToString());
+                }
+                ViewData["list"] = list;
+            }
+            else if (Request.Query["t"] == "teachers")
+            {
+                ViewData["stype"] = "teachers";
+                ViewData["message"] = "List Of Teachers";
+                List<String> list = new List<string>();
+                for (int i = 1; i <= 10; i++)
+                {
+                    list.Add("Teacher " + i.ToString());
+                }
+                ViewData["list"] = list;
+            }
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
+            
---
+++ Section2_26/Section2_26/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Section2_26.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
