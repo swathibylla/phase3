924f75eba6165eccb43bfb3ac24734fc4571f010
swathibylla <swathibylla@gmail.com>
2023-12-05 10:38:41 +0530

    files

---
+++ Section2_14/Section2_14/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Section2_14.Models;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Section2_14.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult FormAction(Student student)
+        {
+            if (ModelState.IsValid)
+                return Content("Form data is valid.");
+            else
+            {
+                StringBuilder sb = new StringBuilder("");
+
+                foreach (ModelStateEntry value in ViewData.ModelState.Values)
+                {
+                    if (value.Errors.Count > 0)
+                    {
+                        for (int i = 0; i < value.Errors.Count; i++)
+                            sb.Append(value.Errors[i].ErrorMessage + "\n");
+                    }
+                }
+                return Content("Form data is invalid with " + ModelState.ErrorCount.ToString() + " errors:\n " + sb.ToString());
+            }
+        }
+    }
+}
+
+
+ 
---
+++ Section2_14/Section2_14/Models/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Section2_14.Models
+{
+    public class Student
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "Name is required")]
+        public string Name { get; set; } = default!;
+
+        [Required]
+        [StringLength(255)]
+        public string Address { get; set; } = default!;
+
+        [Required]
+        [StringLength(10)]
+        public string Class { get; set; } = default!;
+
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string ContactEmail { get; set; } = default!;
+
+        [Required]
+        [Range(5, 14)]
+        public int Age { get; set; }
+
+
+
+    }
+
+}
+
