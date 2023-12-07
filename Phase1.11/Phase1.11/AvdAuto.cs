263c97f75c4bc6e59773feef9d045881dd4d7e44
swathibylla <swathibylla@gmail.com>
2023-12-07 20:54:46 +0530

    files

---
+++ Phase1.11/Phase1.11/AvdAuto.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Internal;
+
+namespace Phase1._11
+{
+    [TestClass]
+    public class AvdAuto
+    {
+        [TestMethod]
+        public void IFrame()
+        {
+            using (var driver = new FirefoxDriver())
+            {
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("file:C:\\Users\\swath\\Documents\\WebConnectivity\\Phase1.11\\Phase1.11\\IFrametest.html");
+
+                new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(
+                     d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
+
+                //var iframes = driver.FindElementsByTagName("Iframe");
+                //Assert.IsTrue(Iframes.Count == 2);
+
+                driver.SwitchTo().Frame(0);
+                var frameTitle = driver.PageSource.Contains("Corporate Training");
+                Assert.IsNotNull(frameTitle);
+            }
+        }
+
+        [TestMethod]
+        public void ScreenShot()
+        {
+            using (var driver = new FirefoxDriver())
+            {
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://www.simplilearn.com");
+
+                new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(
+                     d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
+
+                ITakesScreenshot screenshotDriver = driver as ITakesScreenshot;
+                Screenshot screenshot = screenshotDriver.GetScreenshot();
+                screenshot.SaveAsFile("d:/test.png", ScreenshotImageFormat.Png); 
+            }
+        }
+    }
+
+}
+
---
+++ Phase1.11/Phase1.11/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Phase1.11")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Phase1.11")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("211a3052-67eb-4e32-bebc-21fddf9a2b6c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
