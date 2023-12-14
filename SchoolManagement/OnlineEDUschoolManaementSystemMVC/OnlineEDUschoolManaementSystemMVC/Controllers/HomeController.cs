0221b33458f9d3dd81851121eff5bb1127231648
swathibylla <swathibylla@gmail.com>
2023-12-14 19:25:29 +0530

    files

---
+++ SchoolManagement/OnlineEDUschoolManaementSystemMVC/OnlineEDUschoolManaementSystemMVC/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using OnlineEDUschoolManaementSystemMVC.Models;
+
+namespace OnlineEDUschoolManaementSystemMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Courses()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+
+
+
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registration(Student r)
+        {
+            using (SchoolsystemEntities db = new SchoolsystemEntities())
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Students.Add(r);
+                    db.SaveChanges();
+                    //  ViewBag.message("Register Sucess");
+                    ModelState.Clear();
+                }
+            }
+            return View(r);
+        }
+        [HttpPost]
+        public ActionResult Login(Student r1)
+        {
+            using (SchoolsystemEntities db = new SchoolsystemEntities())
+            {
+                if (ModelState.IsValid)
+                {
+                    var log = db.Students.Where(a => a.Name.Equals(r1.Name) && a.studentId.Equals(r1.studentId)).FirstOrDefault();
+                    if (log != null)
+                    {
+                        return RedirectToAction("Contact");
+                    }
+                    db.SaveChanges();
+                    // ViewBag.message("Login Sucess");
+                    ModelState.Clear();
+                }
+            }
+            return View(r1);
+        }
+
+    }
+}
---
+++ SchoolManagement/OnlineEDUschoolManaementSystemMVC/OnlineEDUschoolManaementSystemMVC/Data/DbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace OnlineEDUschoolManaementSystemMVC.Data
+{
+    public class SchoolSystemEntities: DbContext
+    {
+        public SchoolSystemEntities(): base("name=SchoolSystemEntities")
+        {
+
+        }
+        public DbSet<SchoolSystemEntities> Student
+        { get; set; }
+    }
+}
